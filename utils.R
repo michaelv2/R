72150d06c346e303d4e05ac124643643d6d9e8e9
michaelv2 <michael.wagg@gmail.com>
2013-09-17 20:16:34 -0400

    Create utils.R

---
+++ utils.R
@@ -0,0 +1,56 @@
+rle2 <- function(x) {
+  # Modified version of base::rle that includes indexes
+  if (!is.vector(x) && !is.list(x)) 
+    stop("'x' must be an atomic vector")
+  n <- length(x)
+  if (n == 0L) 
+    return(structure(list(lengths = integer(), values = x), 
+                     class = "rle"))
+  y <- x[-1L] != x[-n]
+  i <- c(which(y | is.na(y)), n)
+  data.frame(lengths = diff(c(0L, i)), values = x[i], index = i)
+}
+
+getRuns <- function(x, decreasing=F) {
+  # Function to calculate runs of equal values for every element in a vector
+  if (decreasing) {
+    x <- rev(x)
+  }
+  runs <- list()
+  done <- FALSE
+  while (!done) {
+    y <- rle2(x)
+    y <- subset(y, values!=0)
+    if (nrow(y)>0) {
+      runs[[length(runs)+1]] <- y
+      x[y[,'index']] <- 0
+    } else {
+      done <- TRUE
+    }
+  }
+  runs <- do.call(rbind, runs)
+  if (decreasing) {
+    runs <- runs[order(runs$index, decreasing=T),]
+    runs$index <- rev(runs$index)
+  }
+  runs <- runs[order(runs$index),]
+  return(runs)
+}
+
+get.bday <- function(startDt, endDt, region='US') {
+  # Return business days for given date range
+  library(timeDate)
+  
+  dtes <- seq(as.Date(as.character(startDt), '%Y%m%d'), 
+              as.Date(as.character(endDt), '%Y%m%d'), by=1)
+  dtes <- as.timeDate(dtes)
+  if (region=='US') {
+    dtes <- dtes[isBizday(dtes, holidays=holidayNYSE(), wday=1:5)]
+  } else if (region=='LN') {
+    dtes <- dtes[isBizday(dtes, holidays=holidayLONDON(), wday=1:5)]
+  }
+  
+  return(strftime(dtes, '%Y%m%d'))
+}
+
+
