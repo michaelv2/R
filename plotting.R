e290102478770ca0a8e89b82c78f14f32a9c6d5f
michaelv2 <michael.wagg@gmail.com>
2015-09-18 22:58:40 -0400

    Create plotting.R

---
+++ plotting.R
@@ -0,0 +1,15 @@
+clrs.spec <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
+clrs.hcl <- function(n) {
+  hcl(h = seq(230, 0, length.out = n), 
+      c = 60, l = seq(10, 90, length.out = n), 
+      fixup = TRUE)
+}
+
+### function to plot a colour palette
+pal <- function(col, border = "transparent", ...)
+{
+  n <- length(col)
+  plot(0, 0, type="n", xlim = c(0, 1), ylim = c(0, 1),
+       axes = FALSE, xlab = "", ylab = "", ...)
+  rect(0:(n-1)/n, 0, 1:n/n, 1, col = col, border = border)
+}
